5f93a949657603d0308d04b6370233d763ed9997
pranab <pranav006@gmail.com>
2024-08-16 10:56:19 +0530

    Fithcommit

--- Sorting.cpp
+++ Sorting.cpp
@@ -1,81 +1,150 @@
-// Sorting using Bubble
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-void swap(int *x,int *y){
-    int temp=*x;
-    *x=*y;
-    *y=temp;
+
+void swap(int *x, int *y) {
+    int temp = *x;
+    *x = *y;
+    *y = temp;
 }
-void Bubblesorting(int A[],int n){
-    for(int i=0;i<n-1;i++){
-        for(int j=0;j<n-i-1;j++){
-            if(A[j]>A[j+1])
-                swap(&A[j],&A[j+1]);
+
+// Bubble Sort
+void Bubblesorting(int A[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (A[j] > A[j + 1])
+                swap(&A[j], &A[j + 1]);
         }
     }
 }
-//sorting using insertionsort
-void insertionsort(int A[],int n){
-    int x;
-    for(int i=1;i<n;i++){
-        x=A[i];
-        int j=i-1;
-        while(j>=0&&A[j]>x){
-            A[j+1]=A[j];
+
+// Insertion Sort
+void insertionsort(int A[], int n) {
+    for (int i = 1; i < n; i++) {
+        int x = A[i];
+        int j = i - 1;
+        while (j >= 0 && A[j] > x) {
+            A[j + 1] = A[j];
             j--;
-
         }
-        A[j+1]=x;
+        A[j + 1] = x;
     }
 }
-//Sorting using Selection Sort
-void selectionsort(int A[],int n){
-    int i,k,j;
-    for(i=0;i<n-1;i++){
-        for(j=k=i;j<n;j++){
-            if(A[j]<A[k]){
-                k=j;
+
+// Selection Sort
+void selectionsort(int A[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int k = i;
+        for (int j = i + 1; j < n; j++) {
+            if (A[j] < A[k]) {
+                k = j;
             }
-            swap(&A[i],&A[k]);
         }
+        swap(&A[i], &A[k]);
     }
 }
-//Sorting using Quick Sort
-int partion(int A[],int l,int h){
-    int pivot=A[l];
-    int i=l,j=h;
-    do{
-        do{i++;}while(i<=h&&A[i]<=pivot);
-        do{j--;}while(A[j]>pivot);
-        if(i<j){
-            swap(&A[i],&A[j]);
-        }
 
-    }while(i<j);
-    swap(&A[l],&A[j]);
+// Quick Sort
+int partition(int A[], int l, int h) {
+    int pivot = A[l];
+    int i = l + 1;
+    int j = h;
+
+    while (true) {
+        while (i <= h && A[i] <= pivot) i++;
+        while (j > l && A[j] > pivot) j--;
+
+        if (i >= j) break;
+        swap(&A[i], &A[j]);
+    }
+    swap(&A[l], &A[j]);
     return j;
 }
-void Quicksort(int A[],int l,int h){
-    int j;
-    if(l<h){
-        j=partion(A,l,h);
-        Quicksort(A,l,j);
-        Quicksort(A,j+1,h);
 
+void Quicksort(int A[], int l, int h) {
+    if (l < h) {
+        int j = partition(A, l, h);
+        Quicksort(A, l, j - 1);
+        Quicksort(A, j + 1, h);
     }
 }
-int main(){
-    int A[] = {50, 70, 60, 90, 40, 80, 10, 20, 30};
-    int n=sizeof(A)/sizeof(A[0]);
-    // Bubblesorting(A,n);
-    // insertionsort(A,n);
-    // selectionsort(A,n);
-    Quicksort(A,0,n);
-    
-    
-    for(int i=0;i<n;i++){
-        cout<<A[i]<<" ";
 
+// Merge Sort
+void mergesort(int A[], int B[], int n, int m) {
+    int c[n + m];
+    int i = 0, j = 0, k = 0;
+
+    while (i < n && j < m) {
+        if (A[i] < B[j]) {
+            c[k++] = A[i++];
+        } else {
+            c[k++] = B[j++];
+        }
     }
+    while (i < n) c[k++] = A[i++];
+    while (j < m) c[k++] = B[j++];
+
+    for (i = 0; i < n + m; i++) {
+        cout << c[i] << " ";
+    }
+    cout << endl;
+}
+
+// Count Sort
+int maxelement(int A[], int n) {
+    int max = A[0];
+    int i = 1;
+
+    while (i < n) {
+        if (A[i] > max) {
+            max = A[i];
+        }
+        i++;
+    }
+    return max;
+}
+
+void countsort(int A[], int n) {
+    int max = maxelement(A, n);
+    int *c = new int[max + 1];
+
+    int i = 0;
+    while (i <= max) {
+        c[i] = 0;
+        i++;
+    }
+
+    i = 0;
+    while (i < n) {
+        c[A[i]]++;
+        i++;
+    }
+
+    i = 0;
+    int j = 0;
+    while (i <= max) {
+        while (c[i] > 0) {
+            A[j++] = i;
+            c[i]--;
+        }
+        i++;
+    }
+
+    delete[] c;
+}
+
+int main() {
+    int A[] = {50, 30, 30, 34, 60, 90};
+    int B[] = {10, 20, 30, 40, 80};
+    int n = sizeof(A) / sizeof(A[0]);
+    int m = sizeof(B) / sizeof(B[0]);
+
+    // mergesort(A, B, n, m);
+    countsort(A, n);
+
+    for (int i = 0; i < n; i++) {
+        cout << A[i] << " ";
+    }
+    cout << endl;
+
     return 0;
 }
